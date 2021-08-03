ac7b4e019e2e4ab187a0ad2f6554a6af781745d9
Amit Kundu <kundu.amit517@gmail.com>
2021-08-03 15:20:28 +0600

    Delete task & prop drilling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
   const name = "Brad";
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      reminder: true,
+      day: "Monday 1 PM",
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      reminder: true,
+      day: "Monday 5 PM",
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      reminder: true,
+      day: "Monday 12 PM",
+    },
+    {
+      id: 4,
+      text: "Doctors Appointment",
+      reminder: true,
+      day: "Monday 05 PM",
+    },
+  ]);
+
+  //Delete task
+
+  const deleteTask = (id) => {
+    setTask(tasks.filter((tasks) => tasks.id !== id));
+    console.log(id);
+  };
 
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      {/* <Tasks tasks={tasks} onDelete={deleteTask} /> */}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} /> : 'No task to show'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,9 @@
-import { useState } from "react";
-
-export const Tasks = () => {
-  const [tasks, setTask] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at school",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food shopping",
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: "Doctors Appointment",
-      reminder: true,
-    },
-  ]);
-
+import { Task } from "./Task";
+export const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
